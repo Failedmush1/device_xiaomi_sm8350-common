5259d22d62edadfb12c22c375f09f86761bc5544
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2024-08-17 13:25:14 +0530

    sm8350-common: Include Moto Dolby Atmos
    * Using dolby from: https://github.com/swiitch-OFF-Lab/hardware_dolby
    * dolby: Add mediacodecs support (Useful for Apple Music)
    
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -100,6 +100,10 @@ TARGET_GRALLOC_HANDLE_HAS_NO_RESERVED_SIZE := true
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@3.0
 
+# Dolby
+$(call inherit-product-if-exists, hardware/dolby/dolby.mk)
+
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
