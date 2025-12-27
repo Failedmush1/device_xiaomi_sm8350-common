8b61c96b801f693ffd2c0e944b0aa6789db093c8
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-27 14:23:05 +0900

    Fix some issues

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,8 +73,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/hidl/manifest_lahaina.xml \
     $(COMMON_PATH)/hidl/manifest_xiaomi.xml
-    $(COMMON_PATH)/hidl/manifest_xiaomi.xml \
-    $(if $(TARGET_NFC_SUPPORTED_SKUS),$(COMMON_PATH)/hidl/manifest_no_nfc.xml,) \
     vendor/oneplus/dolby/vintf/dolby_manifest.xml
 
 ifeq ($(TARGET_HAS_UDFPS),true)
