e9957895e4e94dfb2fc3057a614699ff30eab0a0
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-04-11 09:39:25 +0530

    sm8350-common: Move c2_manifest_vendor.xml to common
    Change-Id: I59ceeec26346c0a04a636fa840218dd75b703b7b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,8 @@ DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/hidl/manifest_udfps.xml
 endif
 
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/hidl/c2_manifest_vendor.xml
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image
