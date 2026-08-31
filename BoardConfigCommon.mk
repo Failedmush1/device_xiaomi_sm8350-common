d0e1cdc55147aae5b2aa18dc081d357a43ae1bcc
Failedmush <anytimewicked@gmail.com>
2026-08-31 04:22:47 +0000

    sm8350-common: Fix libion sepolicy include path
    Change device/hertzify to device/lineage

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,7 +173,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2025-11-01
 
 # Sepolicy
-include device/hertzify/sepolicy/libion/sepolicy.mk
+include device/lineage/sepolicy/libion/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
